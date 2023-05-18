071134ba74c883a13e173efcbc30d89d2b6e60cc
allyleandre <tuyambazeleandre@gmail.com>
2023-05-18 20:27:27 +0200

    enable cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express, { application } from "express";
 import morgan from "morgan";
+import cors from "cors";
 import allRoutes from "./routes/index";
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("combined"));
 app.use(express.json());
 
